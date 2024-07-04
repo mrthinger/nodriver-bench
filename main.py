fce17d212fd4a6005348a7826928d464d996bf09
Evan Pierce <evan@mrthinger.com>
2024-07-03 22:11:46 -0700

    blocked fast

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-import asyncio
 import nodriver as uc
+import time
 
 async def main():
-    browser = await uc.start()
+    start_time = time.time()
+
+    browser = await uc.start(sandbox=False, headless=True)
     page = await browser.get('https://www.google.com')
 
     search_input = await page.select('textarea[name="q"]')
-    
-    # Type 'multion' into the search field
     await search_input.send_keys("multion")
-    
-    # Optional: Press Enter to perform the search
-    
+
     search = await page.find('Google Search', best_match=False)
     await search.click()
 
     content = await page.get_content()
     print(content)
 
+    if 'We are a technology and design group' in content:
+        print("success")
+    else:
+        print("blocked")
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Execution time: {elapsed_time:.2f} seconds")
 
 
 if __name__ == '__main__':
